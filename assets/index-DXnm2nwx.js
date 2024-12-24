b0b813c1dba034673bada67e309997b31684d523
Dave Gonzalez <dave.gonzolaz@bgis.com>
2024-12-23 23:26:11 -0500

    Updates

--- assets/index-DXnm2nwx.js
+++
@@ -1,1 +0,0 @@
-import{j as t,S as e,O as r}from"./index-D8gHp1wz.js";const s=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",bgcolor:"neutral.lighter",children:t.jsx(r,{})});export{s as default};
